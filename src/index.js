e12d4c208f6c8223ade62bba41bc580be29ace87
Anoop21-oss-a <anoopkumarpatel935@gmail.com>
2024-09-11 01:34:42 +0530

    added unique constraint and started city repositry

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const express=require("express")
 const bodyparser=require("body-parser")
-
+const {City}=require('./models/index')
 const{PORT}=require('./config/ServerConfig');
+const CityRepositry=require('./repositry/city-repositry')
 
 const setupserver=async()=>{
     const app=express();
     app.use(bodyparser.json())
     app.use(bodyparser.urlencoded({extended:true}))
-    app.listen(PORT,()=>{
+    app.listen(PORT,async()=>{
         console.log(`server started at ${PORT}`);
+        const repo=new CityRepositry();
+        repo.createCity({name: "bho pal"});
     });
 }
 setupserver();
